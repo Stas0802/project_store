0a6f3f05da02bc6c8d22e6493b8160c081ef6d7c
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:22:21 +0300

    add create.blade for order page

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -9,22 +9,34 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
                     <label for="costumer_name" class="block text-sm font-medium">Имя</label>
-                    <input type="text" name="costumer_name" id="costumer_name" class="mt-1 w-full border rounded p-2" required>
+                    <input type="text" name="costumer_name"
+                           id="costumer_name" value="{{ old('costumer_name') }}"
+                           class="input-base {{ $errors->has('costumer_name') ? 'input-error' : 'input-valid' }}">
+                    <x-input-error :messages="$errors->get('costumer_name')"/>
                 </div>
 
                 <div>
                     <label for="costumer_surname" class="block text-sm font-medium">Фамилия</label>
-                    <input type="text" name="costumer_surname" id="costumer_surname" class="mt-1 w-full border rounded p-2" required>
+                    <input type="text" name="costumer_surname"
+                           id="costumer_surname" value="{{ old('costumer_surname') }}"
+                           class="input-base {{ $errors->has('costumer_surname') ? 'input-error' : 'input-valid'}}">
+                    <x-input-error :messages="$errors->get('costumer_surname')"/>
                 </div>
 
                 <div class="md:col-span-2">
                     <label for="phone_number" class="block text-sm font-medium">Телефон</label>
-                    <input type="text" name="phone_number" id="phone_number" class="mt-1 w-full border rounded p-2" required>
+                    <input type="text" name="phone_number"
+                           id="phone_number" value="{{ old('phone_number') }}"
+                           class="input-base {{ $errors->has('phone_number') ? 'input-error' : 'input-valid'}}">
+                    <x-input-error :messages="$errors->get('phone_number')"/>
                 </div>
 
                 <div class="md:col-span-2">
                     <label for="delivery" class="block text-sm font-medium">Адрес доставки</label>
-                    <input type="text" name="delivery" id="delivery" class="mt-1 w-full border rounded p-2" required>
+                    <input type="text" name="delivery"
+                           id="delivery" value="{{ old('delivery') }}"
+                           class="input-base {{ $errors->has('delivery') ? 'input-error' : 'input-valid' }}">
+                    <x-input-error :messages="$errors->get('delivery')"/>
                 </div>
             </div>
 
