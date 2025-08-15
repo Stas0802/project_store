7b3e1788e5d73aff7dc7e23eaef07163b1c90c3f
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:27:58 +0300

    add index.blade for show all categories on page

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -1,18 +1,27 @@
-@extends('layouts.main')
+@extends('layouts.main', ['sidebar' => true])
 
 @section('title', 'Категории')
 
 @section('content')
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-6 px-4 py-8">
-        @foreach($categories as $category)
-            <a href="{{ route('category.product', $category) }}">
-                <div class="flex flex-col items-center text-center space-y-3 bg-white rounded-lg shadow p-4 w-48 h-52"
-                >
-                    <img src="{{ asset('storage/category/' . $category->image) }}" alt="{{ $category->name }}"
-                         class="w-full h-40 object-cover rounded">
-                    <p class="mt-3 text-center font-semibold text-gray-800">{{ $category->name }}</p>
-                </div>
-            </a>
-        @endforeach
+    <div class="flex justify-end">
+        <div class="flex flex-wrap justify-end gap-12">
+            @foreach($categories as $category)
+                <a href="{{ route('category.product', $category) }}" class="sm:w-80 md:w-96">
+                    <div class="relative group h-52 overflow-hidden rounded-lg shadow">
+                        <img src="{{ asset('storage/category/' . $category->image) }}"
+                             alt="{{ $category->name }}"
+                             class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105">
+                        {{-- Верхний прозрачный козырёк с названием --}}
+                        <div class="absolute top-0 left-0 w-full h-1/5 bg-black bg-opacity-40 flex items-center px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <p class="text-white text-lg  font-semibold">{{ $category->name }}</p>
+                        </div>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
+    {{--  links paginate  --}}
+    <div class="mt-4 ml-12 w-full flex justify-center">
+        {{ $categories->links() }}
     </div>
 @endsection
