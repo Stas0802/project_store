41b8d1ef16923444abcc8ec5e7e840c38e8170a7
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:24:53 +0300

    change edit.blade for edite user page

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -10,14 +10,25 @@
             @csrf
             {{method_field('PATCH')}}
 
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" value="{{$user->name}}" type="text" required class="mt-1 block w-full" />
+            <x-input-label for="user-name" :value="__('Name')" />
+            <x-text-input id="user-name" name="user_name" value="{{$user->name}}" type="text"
+                          class="input-base {{ $errors->getBag('user')->has('user_name') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->getBag('user')->get('user_name')"/>
 
             <x-input-label for="email" :value="__('Email')" class="mt-4" />
-            <x-text-input id="email" name="email" value="{{$user->email}}" type="email" required class="mt-1 block w-full" />
+            <x-text-input id="email" name="email" value="{{$user->email}}" type="email"
+                          class="input-base {{ $errors->getBag('user')->has('email') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->getBag('user')->get('email')"/>
 
             <x-input-label for="password" :value="__('New Password')" class="mt-4" />
-            <x-text-input id="password" name="password"  type="password" required class="mt-1 block w-full" />
+            <x-text-input id="password" name="password"  type="password"
+                          class="input-base {{ $errors->getBag('user')->has('password') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->getBag('user')->get('password')"/>
+
+            <x-input-label for="password-confirm" :value="__('Password confirm')" class="mt-4" />
+            <x-text-input id="password-confirm" name="password_confirmation" type="password" value="{{ old('password_confirmation') }}"
+                          class="input-base {{ $errors->getBag('user')->has('password_confirmation') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->getBag('user')->get('password_confirmation')" class="mt-2"/>
 
             <x-primary-button class="mt-4">
                 {{ __('Edite User') }}
