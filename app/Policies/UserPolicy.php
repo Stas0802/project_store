1f1741fb8fd071123e42d34a9d6e2e01c04a0a44
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:38:18 +0300

    add class UserPolicy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -36,7 +36,10 @@ class UserPolicy
      */
     public function update(User $user, User $model): bool
     {
-        return $user->is_admin && $user->id !== $model->id;
+         if($user->is_admin){
+             return true;
+         }
+         return $user->id === $model->id;
     }
 
     /**
