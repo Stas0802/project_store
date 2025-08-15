3b73e8a4cdd36c7bbf92fbe3fe8c3dfafec04036
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:22:00 +0300

    add create.blade for category page

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -8,15 +8,18 @@
     <div class="py-12">
         <form method="POST" action="{{ route('category.store') }}" enctype="multipart/form-data" class="max-w-xl mx-auto">
             @csrf
-            <!-- Название -->
+
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" required class="mt-1 block w-full" />
+            <x-text-input id="name" name="name" type="text" value="{{ old('name') }}"
+                          class="input-base {{ $errors->has('name') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->get('name')"/>
 
-            <!-- Картинка -->
+
             <x-input-label for="image" :value="__('Image')" class="mt-4" />
-            <x-text-input id="image" name="image" type="file" required class="mt-1 block w-full" />
+            <x-text-input id="image" name="image" type="file"  class="mt-1 block w-full" />
+            <x-input-error :messages="$errors->get('image')" class="mt-2"/>
 
-            <!-- Кнопка -->
+
             <x-primary-button class="mt-4">
                 {{ __('Create category') }}
             </x-primary-button>
