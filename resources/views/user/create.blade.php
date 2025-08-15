d173d6aa461ba8112f24dfc46eb2c59bad1883b0
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:23:07 +0300

    add create.blade for user page

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,16 +8,25 @@
     <div class="py-12">
         <form method="POST" action="{{ route('user.store') }}" class="max-w-xl mx-auto">
             @csrf
-
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" required class="mt-1 block w-full" />
+            <x-text-input id="name" name="name" type="text" value="{{ old('name') }}"
+                          class="input-base {{ $errors->has('name') ? 'input-error' : 'input-valid'}}"/>
+            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
 
             <x-input-label for="email" :value="__('Email')" class="mt-4" />
-            <x-text-input id="email" name="email" type="email" required class="mt-1 block w-full" />
+            <x-text-input id="email" name="email" type="email" value="{{ old('email') }}"
+                          class="input-base {{ $errors->has('email') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2"/>
 
             <x-input-label for="password" :value="__('Password')" class="mt-4" />
-            <x-text-input id="password" name="password" type="password" required class="mt-1 block w-full" />
+            <x-text-input id="password" name="password" type="password" value="{{ old('password') }}"
+                          class="input-base {{ $errors->has('password') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2"/>
 
+            <x-input-label for="password-confirm" :value="__('Password confirm')" class="mt-4" />
+            <x-text-input id="password-confirm" name="password_confirmation" type="password" value="{{ old('password_confirmation') }}"
+                          class="input-base {{ $errors->has('password_confirmation') ? 'input-error' : 'input-valid' }}" />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2"/>
             <x-primary-button class="mt-4">
                 {{ __('Create User') }}
             </x-primary-button>
