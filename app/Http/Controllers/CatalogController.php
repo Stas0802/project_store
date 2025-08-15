4b244b0b4c04045e122349f2547dfd6a6c63aae7
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:20:06 +0300

    add class CatalogController

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
+    /**
+     * Show categories Home page and paginate 12 things
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+     */
     public function index(){
-        $categories = Category::all();
+        $categories = Category::paginate(6);
         return view('catalog.index', ['categories' => $categories]);
     }
 
+    /**
+     * Show products in category on page and 12 things
+     * @param Category $category
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+     */
     public function show(Category $category){
 
-        $products = Product::where('categories_id', $category->id)->get();
+        $products = Product::where('category_id', $category->id)
+            ->paginate(6);
         return view('catalog.show', ['category' => $category, 'products' => $products]);
     }
 }
