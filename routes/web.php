458bfd204e0b9d654f7151cc87d4fcad5d525e3d
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:38:58 +0300

    add in web.php Routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderProductController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
@@ -21,14 +23,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/**
+ * Route dashboard
+ */
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['if.not.auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+/**
+ * Rote profile
+ */
+Route::middleware('if.not.auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -37,7 +43,7 @@ Route::middleware('auth')->group(function () {
 /**
  * Route users
  */
-Route::middleware('auth')->group(function () {
+Route::middleware('if.not.auth')->group(function () {
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::post('/user', [UserController::class, 'store'])->name('user.store');
     Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
@@ -49,8 +55,12 @@ Route::middleware('auth')->group(function () {
 /**
  *  Route products and category
  */
-Route::resource('category', CategoryController::class)->middleware('auth');
-Route::resource('product', ProductController::class)->middleware('auth');
+Route::resource('category', CategoryController::class)->middleware('if.not.auth');
+Route::resource('product', ProductController::class)->middleware( 'if.not.auth');
+
+/**
+ * Route content
+ */
 Route::get('/catalog/category/{category}', [CatalogController::class, 'show'])->name('category.product');
 Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
 Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog.index');
@@ -63,10 +73,40 @@ Route::post('/cart/add/{product}', [CartController::class, 'addToCart'])->name('
 Route::delete('/cart/{id}', [CartController::class, 'removeCart'])->name('cart.remove');
 
 /**
- * Route order
+ * Route order and OrderProduct
+ */
+Route::patch('/orderProduct/{order}/products', [OrderProductController::class, 'updateProductInOrder'])
+    ->middleware('if.not.auth')
+    ->name('orderProduct.updateProduct');
+
+Route::delete('/orderProduct/{order}/product/{product}', [OrderProductController::class, 'deleteProductFromOrder'])
+    ->middleware('if.not.auth')
+    ->name('orderProduct.deleteProduct');
+
+Route::get('/orderProduct/{order}', [OrderProductController::class, 'edit'])
+    ->middleware('if.not.auth')
+    ->name('orderProduct.edit');
+
+/**
+ * Route thank you page
+ */
+Route::get('/order/thank-you-page', function (){
+    return view('order.thank-you-page');
+})->name('order.thank-you-page');
+
+Route::resource('order', OrderController::class)->middleware('if.not.auth');
+Route::get('/order/create', [OrderController::class, 'create'])->name('order.create');
+Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+
+/**
+ * Route status update
  */
 
-Route::resource('order', OrderController::class);
+Route::patch('/order/{order}/status',[OrderController::class, 'updateStatus'])
+    ->name('order.updateStatus')
+    ->middleware('if.not.auth');
+
+
 
 
 require __DIR__.'/auth.php';
