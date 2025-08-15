72b6615dbe1c4909748595c69ff81cb3969dc14f
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:36:32 +0300

    add show.blade for show one product on page

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,22 +1,29 @@
-
 @extends('layouts.main')
 
 @section('title', $product->name)
 
 @section('content')
-    <div class="max-w-md mx-auto bg-white p-6 rounded shadow">
-        <img src="{{ asset('storage/product/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-60 object-cover rounded mb-4">
+    <div class="flex flex-col md:flex-row justify-center md:justify-between items-start md:space-x-8 p-4">
+        <div class="w-full md:w-1/2 flex flex-col items-center md:items-start mb-4 md:mb-0">
+            <img src="{{ asset('storage/product/' . $product->image) }}" alt="{{ $product->name }}"
+                 class="w-full max-w-md h-full object-cover rounded shadow mb-4">
+            <h2 class="text-xl font-bold">{{ $product->name }}</h2>
+            <p class="text-lg text-white mt-2">Цена: {{ $product->price }} грн</p>
+        </div>
 
-        <h2 class="text-xl font-bold">{{ $product->name }}</h2>
-        <p class="text-gray-700 mt-2">{{ $product->description }}</p>
-        <p class="text-lg text-green-600 mt-4">Цена: {{ $product->price }} грн</p>
+        <div class="w-full md:w-1/2 flex flex-col space-y-4 items-end">
+            <div class="w-full min-h-96 border bg-white p-6 rounded shadow">
+                <p class="text-gray-700">{{ $product->description }}</p>
+            </div>
 
-        <form action="{{ route('cart.add', $product) }}" method="POST">
-            @csrf
-            <button type="submit"
-                    class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
-                🛒 Добавить в корзину
-            </button>
-        </form>
+            <form action="{{ route('cart.add', $product) }}" method="POST" class="w-full">
+                @csrf
+                <button type="submit"
+                        class="flex items-center gap-2 px-6 py-3 bg-green-500 text-white font-bold rounded-lg hover:bg-green-600 transition-colors duration-300 w-80 justify-center">
+                    <span>Добавить в корзину</span>
+                </button>
+            </form>
+        </div>
+
     </div>
 @endsection
