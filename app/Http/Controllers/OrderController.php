0a13792cdcb9d46f47bdb39f2a5fc775708317a6
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:33:21 +0300

    add class  OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreOrderRequest;
 use App\Models\Order;
+use App\Models\Status;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
 
@@ -20,7 +22,8 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $orders = Order::orderBy('created_at', 'desc')->get();
+        return view('order.index', ['orders' => $orders]);
     }
 
     /**
@@ -34,7 +37,7 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreOrderRequest $request)
     {
         $customerData = [
             'costumer_name' => $request->costumer_name,
@@ -48,38 +51,65 @@ class OrderController extends Controller
         if($this->orderService->createOrder($customerData, $products)){
             session()->forget('cart');
         }
-        return redirect()->route('order.create')->with(['success' => 'product in delivery']);
+        return redirect()->route('order.thank-you-page');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $order = Order::with(['status', 'products.category'])->findOrFail($id);
+        $statuses = Status::all();
+        $total = $this->orderService->calcTotal($order);
+        return view('order.show', ['order' => $order, 'statuses' => $statuses, 'total' => $total]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Order $order)
     {
-        //
+        return view('order.edit-customer', ['order' => $order]);
+    }
+
+    /**
+     * Update status in admin panel
+     * @param Request $request
+     * @param Order $order
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function updateStatus(Request $request, Order $order)
+    {
+        $request->validate([
+            'status_id' => 'required|exists:statuses,id'
+        ]);
+        $order->update(['status_id' => $request->status_id]);
+        return redirect()->route('order.index', $order)->with(['success' => 'status order update']);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreOrderRequest $request, Order $order)
     {
-        //
+        $order->costumer_name = $request->costumer_name;
+        $order->costumer_surname = $request->costumer_surname;
+        $order->phone_number = $request->phone_number;
+        $order->delivery = $request->delivery;
+        $order->save();
+        return redirect()->route('order.show', $order->id)->with(['success' => 'Customer data update success']);
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        $order = Order::findOrFail($id);
+        $order->products()->detach();
+        $order->delete();
+        return redirect()->route('order.index')->with(['success' => 'Order delete']);
     }
 }
