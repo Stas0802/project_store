b03c00ce7bfa02c8589b14423b8e6b6d4e6037a2
Stas0802 <stasshulga6@gamail.com>
2025-08-15 17:36:04 +0300

    add show.blade for show product where category

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -1,19 +1,37 @@
-@extends('layouts.main')
+@extends('layouts.main', ['sidebar' => true])
 
 @section('title', $category->name)
 
 @section('content')
-    <h2 class="text-xl font-bold mb-4">Товары категории: {{ $category->name }}</h2>
+ <div class="flex justify-end">
+     <div class="flex flex-wrap justify-end gap-6">
+         @foreach($products as $product)
+             <div class="bg-white rounded shadow p-4">
+                 <a href="{{ route('product.show', $product) }}">
+                     <div class="w-64 h-64 overflow-hidden rounded mb-2">
+                         <img src="{{ asset('storage/product/' . $product->image) }}"
+                              alt="{{ $product->name }}"
+                              class="w-full h-full object-cover">
+                     </div>
+                     <h3 class="text-sm font-semibold">{{ $product->name }}</h3>
+                 </a>
+                 <div class="text-lg text-gray-800 mt-2 flex items-center justify-between w-full">
+                     <span>Цена: {{ $product->price }} грн</span>
 
-    <div class="grid grid-cols-2 md:grid-cols-3 gap-6">
-        @foreach($products as $product)
-            <a href="{{ route('product.show', $product) }}">
-                <div class="bg-white rounded shadow p-4">
-                    <img src="{{ asset('storage/product/' . $product->image) }}" alt="{{ $product->name }}" class="w-full h-40 object-cover mb-2 rounded">
-                    <h3 class="text-sm font-semibold">{{ $product->name }}</h3>
-                    <p class="text-gray-600 text-sm">Цена: {{ $product->price }} грн</p>
-                </div>
-            </a>
-        @endforeach
-    </div>
+                 <form action="{{ route('cart.add', $product) }}" method="POST">
+                     @csrf
+                     <button type="submit"
+                             class="text-2xl text-green-500 hover:text-green-700 transition-colors duration-300"
+                             title="Добавить в корзину">&#128722;
+                     </button>
+                 </form>
+                 </div>
+             </div>
+         @endforeach
+     </div>
+ </div>
+{{-- links paginate --}}
+ <div class="mt-4 w-full flex justify-center">
+     {{ $products->links() }}
+ </div>
 @endsection
